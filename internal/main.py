9f90e41b5b5a49944f50bf2d505661858f3b8100
Eduard Zaripov <edikgoose@email.com>
2024-04-18 01:27:21 +0300

    Add first version of REST API

--- internal/main.py
+++ internal/main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
 
 from fastapi import FastAPI
-from item.router import router as item_router
-from user.router import router as user_router
+from .item.router import router as item_router
+from .user.router import router as user_router
 
-prefix_api = "/api/v1"
 app = FastAPI()
-app.include_router(item_router, prefix=prefix_api)
-app.include_router(user_router, prefix=prefix_api)
+app.include_router(item_router)
+app.include_router(user_router)

dab5c385b2b529dc90e521d04fa1409c091cdbe6
Artem Murashko <art22m@yandex-team.ru>
2024-04-27 00:25:48 +0300

    fix error printing

--- internal/ui/auth.py
+++ internal/ui/auth.py
@@ -8,7 +8,7 @@ pb_auth = pyrebase.initialize_app(
 ).auth()
 
 
-def create_user_with_email_and_password(email, password):
+def create_user_with_email_and_password(email, password):  # TODO: fixme
     request_ref = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key={0}".format(
         st.secrets['FIREBASE_WEB_API_KEY'])
     headers = {"content-type": "application/json; charset=UTF-8"}
@@ -45,10 +45,7 @@ def sign_in(email: str, password: str) -> None:
 
     except requests.exceptions.HTTPError as error:
         error_message = json.loads(error.args[1])['error']['message']
-        if error_message in {"INVALID_EMAIL", "EMAIL_NOT_FOUND", "INVALID_PASSWORD", "MISSING_PASSWORD"}:
-            st.session_state.auth_warning = 'Error: Use a valid email and password'
-        else:
-            st.session_state.auth_warning = 'Error: Please try again later'
+        st.session_state.auth_warning = error_message
 
     except Exception as error:
         print(error)
@@ -57,22 +54,13 @@ def sign_in(email: str, password: str) -> None:
 
 def create_account(email: str, password: str) -> None:
     try:
-        # Create account (and save id_token)
         id_token = create_user_with_email_and_password(email, password)['idToken']
-
-        # Create account and send email verification
         pb_auth.send_email_verification(id_token)
         st.session_state.auth_success = 'Check your inbox to verify your email'
 
     except requests.exceptions.HTTPError as error:
         error_message = json.loads(error.args[1])['error']['message']
-        if error_message == "EMAIL_EXISTS":
-            st.session_state.auth_warning = 'Error: Email belongs to existing account'
-        elif error_message in {"INVALID_EMAIL", "INVALID_PASSWORD", "MISSING_PASSWORD", "MISSING_EMAIL",
-                               "WEAK_PASSWORD"}:
-            st.session_state.auth_warning = 'Error: Use a valid email and password'
-        else:
-            st.session_state.auth_warning = 'Error: Please try again later'
+        st.session_state.auth_warning = error_message
 
     except Exception as error:
         print(error)
@@ -86,13 +74,9 @@ def reset_password(email: str) -> None:
 
     except requests.exceptions.HTTPError as error:
         error_message = json.loads(error.args[1])['error']['message']
-        if error_message in {"MISSING_EMAIL", "INVALID_EMAIL", "EMAIL_NOT_FOUND"}:
-            st.session_state.auth_warning = 'Error: Use a valid email'
-        else:
-            st.session_state.auth_warning = 'Error: Please try again later'
-
-    except Exception:
-        st.session_state.auth_warning = 'Error: Please try again later'
+        st.session_state.auth_warning = error_message
+    except Exception as e:
+        st.session_state.auth_warning = str(e)
 
 
 def sign_out() -> None:
@@ -112,7 +96,7 @@ def delete_account(password: str) -> None:
 
     except requests.exceptions.HTTPError as error:
         error_message = json.loads(error.args[1])['error']['message']
-        print(error_message)
+        st.session_state.auth_warning = error_message
 
     except Exception as error:
         print(error)

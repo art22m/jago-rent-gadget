7c1aa21d4306c8df1a6a4447f2d9f0cd1b4ba1c9
Eduard Zaripov <edikgoose@email.com>
2024-04-29 13:13:36 +0300

    Add some steps for unit tests

--- tests/unit/item/unit_test_item.py
+++ tests/unit/item/unit_test_item.py
@@ -1,36 +1,12 @@
 import unittest
 from unittest.mock import Mock
 
-from sqlalchemy.orm import Session
-
 from internal.item import service
 from internal.item.models import Item
+from tests.unit.config.database import init_db
 
 
-class ItemServiceTests(unittest.TestCase):
-    def setUp(self):
-        self.db_session_mock = Mock(spec=Session)
 
-    # not working
-    def test_get_item_existing_item(self):
-        # Mock the database session and item
-        item_mock = Mock()
-        self.db_session_mock.query().filter().first().return_value = create_item()
-
-        item = service.get_item(self.db_session_mock, 1)
-
-        self.assertEqual(item.id, item_mock.id)
-
-
-def create_item():
-    item = Item()
-    item.id = 1
-    item.owner_id = 1
-    item.title = "title"
-    item.price = 150
-    item.description = "description"
-    item.s3_url = "http://"
-    return item
 
 if __name__ == '__main__':
     unittest.main()
---
+++ tests/unit/user/unit_test_user.py
@@ -0,0 +1,6 @@
+import unittest
+
+
+class UserTests(unittest.TestCase):
+    def test_create_user(self):
+        pass

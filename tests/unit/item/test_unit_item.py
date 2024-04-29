cca4e1deafe0a03385978148d7f05b4bd948d633
Eduard Zaripov <edikgoose@email.com>
2024-04-29 22:06:55 +0300

    Add push

--- tests/unit/item/test_unit_item.py
+++ tests/unit/item/test_unit_item.py
@@ -29,13 +29,17 @@ class UserTests(unittest.TestCase):
         item = service.get_items_by_user(self.mock, 5)
         assert item == "item"
 
-    @patch("internal.user.service.get_user")
-    def test_get_item_by_user__user_not_found(self, get_user):
-        get_user.side_effect = HTTPException(
-            status_code=404, detail=f"User with id=5 is not found"
-        )
+    def test_get_item_by_user__user_not_found(self):
         self.mock.query.return_value.filter.return_value.all.return_value = "item"
+        self.mock.query.return_value.filter.return_value.first.return_value = None
         with pytest.raises(HTTPException) as e:
             service.get_items_by_user(self.mock, 5)
+        assert str(e.value) == "404: User with id=5 is not found"
+
+    def test_create_item(self):
+        self.mock.query.return_value.filter.return_value.all.return_value = "item"
+        self.mock.query.return_value.filter.return_value.first.return_value = None
+        with pytest.raises(HTTPException) as e:
+            service.create_item(self.mock, 5)
         assert str(e.value) == "404: User with id=5 is not found"
 

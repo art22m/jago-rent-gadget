4351b3c2075a4c6351c5db151151c2ada4053128
Eduard Zaripov <edikgoose@email.com>
2024-04-29 14:17:42 +0300

    Remove db init from unit tests

--- tests/unit/item/unit_test_item.py
+++ tests/unit/item/unit_test_item.py
@@ -1,12 +1,4 @@
 import unittest
-from unittest.mock import Mock
-
-from internal.item import service
-from internal.item.models import Item
-from tests.unit.config.database import init_db
-
-
-
 
 if __name__ == '__main__':
     unittest.main()

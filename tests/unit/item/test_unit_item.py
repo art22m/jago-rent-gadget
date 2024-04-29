f67b6ef4f676a3cb1bec44bbbed6dbf22d7a1727
Eduard Zaripov <edikgoose@email.com>
2024-04-29 21:57:46 +0300

    Add unit tests for items

--- tests/unit/item/test_unit_item.py
+++ tests/unit/item/test_unit_item.py
@@ -25,7 +25,17 @@ class UserTests(unittest.TestCase):
     @patch("internal.user.service.get_user")
     def test_get_item_by_user(self, get_user):
         get_user.return_value = "user"
-        self.mock.query.return_value.filter.return_value.first.return_value = "item"
+        self.mock.query.return_value.filter.return_value.all.return_value = "item"
         item = service.get_items_by_user(self.mock, 5)
         assert item == "item"
+
+    @patch("internal.user.service.get_user")
+    def test_get_item_by_user__user_not_found(self, get_user):
+        get_user.side_effect = HTTPException(
+            status_code=404, detail=f"User with id=5 is not found"
+        )
+        self.mock.query.return_value.filter.return_value.all.return_value = "item"
+        with pytest.raises(HTTPException) as e:
+            service.get_items_by_user(self.mock, 5)
+        assert str(e.value) == "404: User with id=5 is not found"
 
